a90378c9d78e93216d89d21dfa93bc29acebf75a
zhangrun <1139135655@qq.com>
2022-11-05 03:17:04 +0800

    NEW
    Signed-off-by: zhangrun <1139135655@qq.com>

--- map.py
+++ map.py
@@ -39,18 +39,19 @@ with open("airport.json", "r", encoding='utf-8') as zones:
     lat_lng = json.load(zones)
 
 
-lat_1 = [0]
-lat_2 = [1]
+lat_1 = 0.1
+lat_2 = 0.1
 dis = 55000
+num = 0
 for zone in lat_lng:
     if (num % 3 == 0):
-        lat_1 = zone
+        lat_1 = zone[0]
     elif (num % 3 == 1):
-        lat_2 = zone
+        lat_2 = zone[0]
     elif (num % 3 == 2):
         name = zone
-        print(lat, dis, name)
-        folium.Circle(location = lat,
+        print(lat_1, lat_2, name)
+        folium.Circle(location = [lat_1, lat_2],
             radius = dis,
             color = 'blue',
             fill = True,).add_to(map)
