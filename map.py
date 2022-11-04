2e20557117f6ed33e67d7f8ec06aeb8c962f0f01
zhangrun <1139135655@qq.com>
2022-11-04 23:06:23 +0800

    NEW
    Signed-off-by: zhangrun <1139135655@qq.com>

--- map.py
+++ map.py
@@ -1,7 +1,7 @@
 import folium
 import json
 
-with open("special_height_zones.json", "r") as zones:
+with open("special_height_zones.json", "r", encoding='utf-8') as zones:
     lat_lng = json.load(zones)
 
 map = folium.Map(location = [34.12052, 107.256486],
@@ -11,21 +11,27 @@ map = folium.Map(location = [34.12052, 107.256486],
         )
 choose_color = ['red', 'blue', 'green']
 
-num = -1
+num = 0
 lat = [0, 0]
 dis = [0]
+name = ["0"]
 
 for zone in lat_lng:
-    if (num % 2 != 0):
+    if (num % 3 == 0):
         lat = zone
         lat = lat[::-1]
-    else:
+    elif (num % 3 == 1):
         dis = zone
-        print(lat, dis)
+    elif (num % 3 == 2):
+        name = zone
+        print(lat, dis, name)
         folium.Circle(location = lat,
             radius = dis,
             color = 'blue',
             fill = True,).add_to(map)
+        folium.Marker(location = lat,
+            popup = name,
+            icon = folium.Icon(icon = 'cloud')).add_to(map)
     num = num + 1
 map.add_child(folium.LatLngPopup())
 map.save("special_height_zones.html")
