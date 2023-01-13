9ea031accb597cc2067f223041ac630dba5f98fe
musk zhang <musk.zhang@dji.com>
2023-01-13 11:17:26 +0800

    NEW: 添加GEO信息
    Signed-off-by: musk zhang <musk.zhang@dji.com>

---
+++ geo/geo_hash.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <string.h>
+
+void geo_hash(gps_t gps, uint32_t acc)
+{
+    
+}
+
+int main()
+{
+
+    return 0;
+}
---
+++ geo/geo_hash.h
@@ -0,0 +1,10 @@
+#ifndef __GEO_HASH__
+#define __GEO_HASH__
+
+typedef struct
+{
+    double lat;//纬度
+    double lon;//精度
+} gps_t;
+
+#endif
