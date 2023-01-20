6f82ac2b03530aa0f6d51bb721ffd9d4ff3d333e
zhangrun <1139135655@qq.com>
2023-01-20 23:54:57 +0800

    NEW: 添加submodule
    Signed-off-by: zhangrun <1139135655@qq.com>

--- geo/geo_hash.c
+++ geo/geo_hash.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <string.h>
+#include "geo_hash.h"
 
 void geo_hash(gps_t gps, uint32_t acc)
 {
@@ -9,6 +10,6 @@ void geo_hash(gps_t gps, uint32_t acc)
 
 int main()
 {
-
+    printf("test");
     return 0;
 }
